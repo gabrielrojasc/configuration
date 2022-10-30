c5e446b1a65f0e44a24e2cf9ca3a1f30bce7c1e0
Gabriel Rojas <gabo06123@gmail.com>
2022-10-30 14:04:09 -0300

    feat: updated tmux conf and added p map to nvim

--- .config/nvim/lua/mappings/init.lua
+++ .config/nvim/lua/mappings/init.lua
@@ -4,3 +4,4 @@ local opts = { noremap = true, silent = true }
 map("n", "t", ":Ex<CR>", opts)
 map("n", "ff", ":Telescope find_files hidden=true<CR>", opts)
 map("n", "fg", ":Telescope live_grep<CR>", opts)
+map("x", "p", '"_dP', opts)
