ae7fde38eb073a45a6b018934d99a968010c861f
Gabriel Rojas <gabo06123@gmail.com>
2022-11-06 21:38:29 -0300

    feat: optimize copy script

--- .zshrc
+++ .zshrc
@@ -12,9 +12,13 @@ if [ "$TMUX" ] && [ "$VIRTUAL_ENV" ];then
   source "$VIRTUAL_ENV/bin/activate"
 fi
 
-# Highlight the current autocomplete option
-# linux LS_COLORS, made from LSCOLORS=ExFxCxDxBxegedabagacad, with https://geoff.greer.fm/lscolors/
+# Colors
+export CLICOLOR=1
+export LSCOLORS="ExFxCxDxBxegedabagacad"
+# Linux LS_COLORS for zsh completion
 export LS_COLORS="di=1;34:ln=1;35:so=1;32:pi=1;33:ex=1;31:bd=34;46:cd=34;43:su=30;41:sg=30;46:tw=30;42:ow=30;43"
+
+# Highlight the current autocomplete option
 zstyle ':completion:*' list-colors "${(s.:.)LS_COLORS}"
 
 # Tab complete hosts from ssh config
--- copy.sh
+++ copy.sh
@@ -1,23 +1,24 @@
 # files
-cp ~/.profile .
-cp ~/.bash_profile .
-cp ~/.gitconfig .
-cp ~/.pylintrc .
-cp ~/.screenrc .
-cp ~/.tmux.conf .
-cp ~/.vimrc .
-cp ~/.zshrc .
-cp ~/.zshenv .
-cp ~/.zsh_aliases .
+cp ~/.profile . &
+cp ~/.bash_profile . &
+cp ~/.gitconfig . &
+cp ~/.pylintrc . &
+cp ~/.screenrc . &
+cp ~/.tmux.conf . &
+cp ~/.vimrc . &
+cp ~/.zshrc . &
+cp ~/.zshenv . &
+cp ~/.zsh_aliases . &
 
 # directories
-cp -r ~/.vim .
-cp -r ~/.config/nvim .config/
-cp -r ~/magnet/.gitconfig magnet/
+cp -r ~/.vim . &
+cp -r ~/.config/nvim .config/ &
+cp -r ~/magnet/.gitconfig magnet/ &
 
 # dump brew leaves and casks
-brew leaves >brew/leaves
-brew list --cask >brew/casks
+brew leaves >brew/leaves &
+brew list --cask >brew/casks &
 
 # update submodules
-git submodule update --remote --rebase
+git submodule update --remote --rebase &
+wait
