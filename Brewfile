4a9ad446c782f255f9e58540baebac065bc9097c
Gabriel Rojas <gabo06123@gmail.com>
2023-06-13 10:25:29 -0400

    feat: add script to copy db from server

--- Brewfile
+++ Brewfile
@@ -1,7 +1,6 @@
 tap "homebrew/bundle"
 tap "homebrew/cask"
 tap "homebrew/cask-versions"
-tap "homebrew/core"
 tap "homebrew/services"
 tap "teamookla/speedtest"
 brew "bash"
---
+++ magnet/scripts/copy-dpt-db.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+host=${1:-$(basename $(pwd))}
+server=${2:-dev}
+service=${3:-postgres}
+
+psql \
+	-c "drop database if exists \"${PGDATABASE}\";" \
+	-c "create database \"${PGDATABASE}\";" \
+	postgres
+
+ssh $host.$server \
+	"cd $host && docker-compose exec -T $service pg_dump --format=custom" |
+	pg_restore --dbname="${PGDATABASE}" --no-owner --no-acl
+
+./manage.py migrate
+./manage.py createsuperuser --no-input
