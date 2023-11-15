14b6e7a3a10e4d5255bfa61b0bececd07fc6cb10
Gabriel Rojas <gabo06123@gmail.com>
2023-11-15 17:57:54 -0300

    feat: add double quotes to source on sk-back script

--- dcc/scripts/start-sk-back.sh
+++ dcc/scripts/start-sk-back.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 cd /Users/gabriel/dcc/SmartKam/backend
-source $(poetry env info --path)/bin/activate
+source "$(poetry env info --path)/bin/activate"
 tmux new-session -d -s sk-back
 tmux send-keys -t sk-back "drs" Enter
 tmux attach -t sk-back
