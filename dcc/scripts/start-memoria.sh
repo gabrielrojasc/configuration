f13f90fc6c4acaf95db97fb368fe2305ea5c2e0f
Gabriel Rojas <gabo06123@gmail.com>
2023-11-29 00:31:45 -0300

    feat: dcc memoria script

---
+++ dcc/scripts/start-memoria.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+cd "/Users/gabriel/dcc/informe-memoria" || exit 1
+
+code . &
+open /Users/gabriel/Library/CloudStorage/GoogleDrive-gabrielrojas@ug.uchile.cl/My\ Drive/uni/10mo\ semestre/CC6907\ -\ Introducción\ al\ Trabajo\ de\ Título/clases/pdfs/02_Entregas.pdf &
+open /Users/gabriel/Library/CloudStorage/GoogleDrive-gabrielrojas@ug.uchile.cl/My\ Drive/uni/10mo\ semestre/CC6907\ -\ Introducción\ al\ Trabajo\ de\ Título/informe/informe.pdf
+
+wait
+
+tmux new-session -d -s "memoria"
+tmux send-keys -t "memoria" "typst watch main.typ" Enter
+tmux new-window -t "memoria"
+tmux attach -t "memoria"
