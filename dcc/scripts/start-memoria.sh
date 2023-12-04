cb4dd744e8be147c4d357b9326f64aeaf35d605a
Gabriel Rojas <gabo06123@gmail.com>
2023-12-04 00:30:03 -0300

    feat: update defaults

--- dcc/scripts/start-memoria.sh
+++ dcc/scripts/start-memoria.sh
@@ -2,11 +2,9 @@
 
 cd "/Users/gabriel/dcc/informe-memoria" || exit 1
 
-code . &
-open /Users/gabriel/Library/CloudStorage/GoogleDrive-gabrielrojas@ug.uchile.cl/My\ Drive/uni/10mo\ semestre/CC6907\ -\ Introducción\ al\ Trabajo\ de\ Título/clases/pdfs/02_Entregas.pdf &
+code .
+open /Users/gabriel/Library/CloudStorage/GoogleDrive-gabrielrojas@ug.uchile.cl/My\ Drive/uni/10mo\ semestre/CC6907\ -\ Introducción\ al\ Trabajo\ de\ Título/clases/pdfs/02_Entregas.pdf
 open /Users/gabriel/Library/CloudStorage/GoogleDrive-gabrielrojas@ug.uchile.cl/My\ Drive/uni/10mo\ semestre/CC6907\ -\ Introducción\ al\ Trabajo\ de\ Título/informe/informe.pdf
-
-wait
 
 tmux new-session -d -s "memoria"
 tmux send-keys -t "memoria" "typst watch main.typ" Enter
--- set_defaults.sh
+++ set_defaults.sh
@@ -2,3 +2,4 @@ defaults write com.apple.dock tilesize -int 56
 killall Dock
 defaults write com.apple.screencapture location ~/Downloads
 defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
+defaults write -g ApplePressAndHoldEnabled -bool false
